bc5c704908d6f2ad592979ccd63d9f23694fa320
yusei <yusei5884@gmail.com>
2020-01-01 21:18:54 +0900

    各ページuseReactRouterに置き換え

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -3,7 +3,6 @@ import LoginHeader from '../Organisms/Header/LoginHeader'
 import LoginForm from '../../cotainers/Organisms/Form/LoginForm'
 import {userState} from '../../reducers/userReducer'
 import useReactRouter from 'use-react-router'
-// import {withRouter, RouteComponentProps} from 'react-router'
 
 interface PageProps {
     user: userState
@@ -11,14 +10,12 @@ interface PageProps {
 }
 
 const Login : React.FC<PageProps> = (props) => {
-    const { history, location, match } = useReactRouter()
+    const { history } = useReactRouter()
     useEffect(() => {
         const isStorageToken = localStorage.getItem('ticket')? true : false
         if(props.user.login || isStorageToken === true) {
             props.isStorageToken({isLogin:isStorageToken})
             history.push('/top')
-            console.log(location)
-            console.log(match)
         }
     })
     return(
--- client/src/components/pages/Top.tsx
+++ client/src/components/pages/Top.tsx
@@ -2,19 +2,20 @@ import React, {useEffect} from 'react'
 import TopHeader from '../../cotainers/Organisms/Header/TopHeader'
 import TableList from '../../cotainers/Pages/Top/TableList'
 import {userState} from '../../reducers/userReducer'
-import {withRouter, RouteComponentProps} from 'react-router'
+import useReactRouter from 'use-react-router'
 
-interface PageProps extends RouteComponentProps {
+interface PageProps {
     user: userState
     isStorageToken: (props: {isLogin: boolean}) => void
 }
 
 const Top : React.FC<PageProps> = (props) => {
+    const {history} = useReactRouter()
     useEffect(() => {
         const isStorageToken = localStorage.getItem('ticket')? true : false
         if(props.user.login === false || isStorageToken === false) {
             props.isStorageToken({isLogin:isStorageToken})
-            props.history.push('/login')
+            history.push('/login')
         }
     })
 
@@ -26,4 +27,4 @@ const Top : React.FC<PageProps> = (props) => {
     )
 }
 
-export default withRouter(Top)
+export default Top
